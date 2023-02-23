49d2ff0b7c5e773ed4dfd370026f65cd09c7ea17
Jon Andoni Galdos <jonandonigv@gmail.com>
2023-02-23 17:37:43 +0100

    Chirp

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -6,15 +6,16 @@ use App\Models\Chirp;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
+use Illuminate\View\View;
 
 class ChirpController extends Controller
 {
     /**
      * Display a listing of the resource.
      */
-    public function index(): Response
+    public function index(): View
     {
-        return response('Hello world!');
+        return view('chirps.index');
     }
 
     /**
@@ -30,7 +31,19 @@ class ChirpController extends Controller
      */
     public function store(Request $request): RedirectResponse
     {
-        //
+        $validated = $request->validate([
+
+            'message' => 'required|string|max:255',
+
+        ]);
+
+ 
+
+        $request->user()->chirps()->create($validated);
+
+ 
+
+        return redirect(route('chirps.index'));
     }
 
     /**
