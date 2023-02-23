67f8c4143d52d089cd98a51a6b6b53753c042bd9
Jon Andoni Galdos <jonandonigv@gmail.com>
2023-02-23 17:54:49 +0100

    View Chirps

--- app/Models/Chirp.php
+++ app/Models/Chirp.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Chirp extends Model
 {
@@ -14,4 +15,9 @@ class Chirp extends Model
         'message',
 
     ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
